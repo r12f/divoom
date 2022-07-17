a5fb724917c289b28fe6ae06f5c5db0e95648b5c
r12f <r12f.code@gmail.com>
2022-07-17 12:28:21 -0700

    Fix divoom_cli package publish issue.

--- divoom_cli/build.rs
+++ divoom_cli/build.rs
@@ -3,7 +3,7 @@ extern crate winres;
 fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
-        res.set_icon("../assets/Logo-256.ico");
+        res.set_icon("assets/Logo-256.ico");
         res.compile().unwrap();
     }
 }
